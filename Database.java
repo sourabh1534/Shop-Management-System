52031db4cd42988e6d5ee897e01d495d4fd7ddfa
Sourabh Chidar <149478530+sourabh1534@users.noreply.github.com>
2025-08-25 01:48:59 +0530

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,7 @@
+package attr;
+
+public class Database {
+	public static final String HOST_URI = "jdbc:mysql://localhost:3306/shopmanagement";
+	public static final String USER = "root";
+	public static final String PASSWORD = "root";
+}
---
+++ Start.java
@@ -0,0 +1,9 @@
+import activity.*;
+import java.lang.*;
+
+public class Start {
+	public static void main(String args[]) {
+		LoginActivity la = new LoginActivity();
+		la.setVisible(true);
+	}
+}
